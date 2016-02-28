17c3b17fc46fcea414f713651d5f460fa8570857
Konrad <kocasp@gmail.com>
2016-02-28 19:46:06 +0100

    Cap setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,1 @@
-set :stage, :production
-
-# Replace 127.0.0.1 with your server's IP address!
-server '51.254.143.140', user: 'deploy', roles: %w{web app}
+server '164.132.108.165', user: 'deploy', roles: %w{web app db}
